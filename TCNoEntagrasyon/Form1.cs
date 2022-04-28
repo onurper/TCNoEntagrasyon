20299ddad689d1ea1ccfccf8720f2b270ad581c4
Onur PER <75972534+onurper@users.noreply.github.com>
2022-04-28 19:10:42 +0300

    Add project files.

---
+++ TCNoEntagrasyon/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TCNoEntagrasyon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            long tcNo = Convert.ToInt64(txtboxTC.Text);
+            string ad = txtboxAd.Text.ToUpper();
+            string soyad = txtBoxSoyad.Text.ToUpper();
+            int dYil = Convert.ToInt32(txtBoxDYil.Text);
+            KimlikReferans.KPSPublicSoapClient servis = new KimlikReferans.KPSPublicSoapClient();
+            bool teyit = servis.TCKimlikNoDogrula(tcNo, ad, soyad, dYil);
+            if (teyit)
+            {
+                MessageBox.Show("Kimlik Doğru");
+            }
+            else
+            {
+                MessageBox.Show("Kimlik Yanlış");
+            }
+        }
+
+        
+    }
+}
